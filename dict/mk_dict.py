979ed9b3e7350df54b148b51001a1471be6a7c45
JordanEmme <jordan.emme@yahoo.fr>
2020-05-15 12:10:53 +0100

    Added script for spanning fixed letters dictionnaries

---
+++ dict/mk_dict.py
@@ -0,0 +1,14 @@
+#Run this script to get dictionnaries of a given length
+
+f = open("english.txt")
+dico = f.read()
+words = dico.split()
+f.close()
+
+
+for i in range(3,8):
+	new = open("eng{}.txt".format(str(i)), "w+")
+	for word in words:
+		if len(word) == i:
+			new.write(word+"\n")
+	new.close()
